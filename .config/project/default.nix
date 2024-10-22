210beec897394064e2f926e9c6ae311a7ea3d293
Greg Pfeil <greg@technomadic.org>
2024-10-22 10:14:35 -0600

    Sync Flaky template

--- .config/project/default.nix
+++ .config/project/default.nix
@@ -6,33 +6,13 @@
   self,
   supportedSystems,
   ...
-}: let
-  githubSystems = ["macos-13" "ubuntu-22.04" "windows-2022"];
-in {
+}: {
   project = {
     name = "pathway";
     summary = "Type-safe system-independent file path library";
-
-    devPackages = [
-      pkgs.cabal-install
-      pkgs.graphviz
-      ## So cabal-plan(-bounds) can be built in a devShell, since it doesn’t
-      ## work in Nix proper.
-      pkgs.zlib
-    ];
   };
 
-  imports = [
-    (import ./github-ci.nix githubSystems [
-      "pathway"
-      "pathway-internal"
-      "pathway-path"
-      "pathway-quickcheck"
-      "pathway-system"
-    ])
-    ./hackage-publish.nix
-    ./hlint.nix
-  ];
+  imports = [./hlint.nix];
 
   ## dependency management
   services.renovate.enable = true;
@@ -43,104 +23,50 @@ in {
     # This should default by whether there is a .git file/dir (and whether it’s
     # a file (worktree) or dir determines other things – like where hooks
     # are installed.
-    git = {
-      enable = true;
-      ignores = [
-        # Cabal build
-        "dist-newstyle"
-      ];
-    };
+    git.enable = true;
   };
 
   ## formatting
   editorconfig.enable = true;
 
   programs = {
-    treefmt = {
-      enable = true;
-      ## Haskell formatter
-      programs.ormolu.enable = true;
-      settings.formatter.prettier.excludes = ["*/docs/license-report.md"];
-    };
-    vale = {
-      enable = true;
-      excludes = [
-        "*.cabal"
-        "*.hs"
-        "*.lhs"
-        "./cabal.project"
-      ];
-      vocab.${config.project.name}.accept = [
-        "bugfix"
-        "comonad"
-        "conditionalize"
-        "functor"
-        "GADT"
-        "Kleisli"
-        "Kmett"
-      ];
-    };
+    treefmt.enable = true;
+    vale.enable = true;
   };
 
   ## CI
-  services.garnix = {
-    enable = true;
-    builds = {
-      ## TODO: Remove once garnix-io/garnix#285 is fixed.
-      exclude = ["homeConfigurations.x86_64-darwin-example"];
-      include = lib.mkForce (
-        [
-          "homeConfigurations.*"
-          "nixosConfigurations.*"
-        ]
-        ++ flaky.lib.forGarnixSystems supportedSystems (
-          sys:
-            [
-              "checks.${sys}.*"
-              "devShells.${sys}.default"
-              "packages.${sys}.default"
-            ]
-            ++ lib.concatMap (ghc: [
-              "devShells.${sys}.${ghc}"
-              "packages.${sys}.${ghc}_all"
-            ])
-            (self.lib.testedGhcVersions sys)
-        )
-      );
-    };
-  };
+  services.garnix.enable = true;
   ## FIXME: Shouldn’t need `mkForce` here (or to duplicate the base contexts).
   ##        Need to improve module merging.
   services.github.settings.branches.main.protection.required_status_checks.contexts =
     lib.mkForce
-    (["check-bounds"]
+    ([
+        "All Garnix checks"
+        "check-bounds"
+        "check-licenses"
+      ]
       ++ lib.concatMap (sys:
         lib.concatMap (ghc: [
           "build (${ghc}, ${sys})"
           "build (--prefer-oldest, ${ghc}, ${sys})"
         ])
         self.lib.nonNixTestedGhcVersions)
-      githubSystems
-      ++ flaky.lib.forGarnixSystems supportedSystems (sys:
-        lib.concatMap (ghc: [
-          "devShell ${ghc} [${sys}]"
-          "package ${ghc}_all [${sys}]"
-        ])
-        (self.lib.testedGhcVersions sys)
-        ++ [
-          "homeConfig ${sys}-${config.project.name}-example"
-          "package default [${sys}]"
-          ## FIXME: These are duplicated from the base config
-          "check formatter [${sys}]"
-          "check project-manager-files [${sys}]"
-          "check vale [${sys}]"
-          "devShell default [${sys}]"
-        ]));
+      self.lib.githubSystems);
+  services.haskell-ci = {
+    inherit (self.lib) defaultGhcVersion;
+    systems = self.lib.githubSystems;
+    ghcVersions = self.lib.nonNixTestedGhcVersions;
+    cabalPackages = {
+      pathway = "pathway";
+      pathway-internal = "internal";
+      pathway-path = "path";
+      pathway-quickcheck = "quickcheck";
+      pathway-system = "system";
+    };
+    latestGhcVersion = "9.10.1";
+  };
 
   ## publishing
-  # NB: Can’t use IFD on FlakeHub (see DeterminateSystems/flakehub-push#69), so
-  #     this is disabled until we have a way to build Haskell without IFD.
-  services.flakehub.enable = false;
   services.github.enable = true;
   services.github.settings.repository.topics = [];
 }
